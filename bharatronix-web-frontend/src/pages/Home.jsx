f4a141a023da6f1e2813ff6792ca887cd1361bf4
YashTyagi09876 <yashtyagi3357@gmail.com>
2025-05-05 21:33:56 +0530

    HomePge

---
+++ bharatronix-web-frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Section1 from './../Component/HomeComponent/Section1'
+import Section3 from './../Component/HomeComponent/Section3'
+import Section4 from './../Component/HomeComponent/Section4'
+import Section5 from './../Component/HomeComponent/Section5'
+const ProductSlider = () => {
+
+return (
+    <div className="px-4 py-8 w-[95%] mx-auto">
+
+
+      <Section1 />
+      <Section3 />
+      <Section4/>
+      <Section5/>
+    </div>
+  );
+};
+
+export default ProductSlider;
