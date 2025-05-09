18fe7f5a8e956155c63e657840cb0a72763e49bf
YashTyagi09876 <yashtyagi3357@gmail.com>
2025-05-09 09:52:09 +0530

    aapchanges

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import SingleProduct from './Component/HomeComponent/SingleProduct'
 import Cart from './Component/Cart/Cart'
 import Thanku from "./Component/HomeComponent/Thanku";
 import Profile from "./Component/HomeComponent/Profile";
-import { CartProvider } from "./context/CartContext.jsx";
+import { CartProvider } from "./context/CartContext";
+
 
 
 const App = () => { 
